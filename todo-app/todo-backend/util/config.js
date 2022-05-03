a20338d66cd20d5ecd004bab145dfa0a7eea4be4
tikibeni <benjamin.blinnikka@hotmail.com>
2022-05-03 09:06:25 +0300

    Exercise 12.6.

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,4 +1,4 @@
-const MONGO_URL = process.env.MONGO_URL || undefined
+const MONGO_URL = 'mongodb://the_username:the_password@localhost:3456/the_database'
 const REDIS_URL = process.env.REDIS_URL || undefined
 
 module.exports = {
