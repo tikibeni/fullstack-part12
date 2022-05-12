43ed2bf9ece8ffcde65a5f5edbe99c257803f0e6
tikibeni <benjamin.blinnikka@hotmail.com>
2022-05-13 02:51:57 +0300

    Exercise 12.22 Finished

--- my-app/backend/README.md
+++
@@ -1,3 +0,0 @@
-Herokun sovellukseen [tästä](http://infinite-dusk-85714.herokuapp.com)
-
-Muihin tekemiini Fullstack-osiin [tästä](https://github.com/tikibeni/fullstack)
--- my-app/frontend/src/services/persons.js
+++ my-app/frontend/src/services/persons.js
@@ -1,25 +1,23 @@
 // Palvelukomponentti, joka vastaa palvelimen kanssa kommunikoinnista
-import axios from 'axios'
-
-const baseUrl = process.env.REACT_APP_BACKEND_URL
+import axios from '../util/apiClient'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
+    const request = axios.get('/persons')
     return request.then(response => response.data)
 }
 
 const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
+    const request = axios.post('/persons', newObject)
     return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    const request = axios.put(`/persons/${id}`, newObject)
     return request.then(response => response.data)
 }
 
 const remove = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
+    const request = axios.delete(`/persons/${id}`)
     return request.then(response => response.data)
 }
 
